b37b823362f0136993c1637880bd8cd623d5d755
Nancy McBride <njmcbride224@gmail.com>
2020-12-19 07:27:18 -0600

    adding variables to js

--- script.js
+++ script.js
@@ -14,6 +14,43 @@ var SpecialCharacter = ["!","@.","#","$","%","*","(",")","_","-","=","+","~","`"
 var UpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var LowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
+//Prompt functions
+
+function generateOptions(){
+  var Length = parseInt(prompt("How many characters would you like in your password?"));
+
+  if (Length <8){
+    alert('Password must be at least 8 characters!');
+    return; }
+
+  if(Length >128){
+    alert('Password must be less than 129 characters!');
+    return; }
+    
+  var confirmSpecialCharacter = confirm("Do you want to include special characters in your password?");
+  var confirmNumeric = confirm("Do you want numbers in your password?");
+  var confirmUpperCase = confirm("Do you want Upper Case letters in your password?");
+  var confirmationLowerCase = confirm("Do you want lower case letters in your password?");
+
+  if (!confirmNumeric && !confirmSpecialCharacter && !confirmUpperCase && !confirmLowerCase){alert("Your password must contain at least 1 special character, number, Upper Case or lower case value!");
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 // Write password to the #password input
 function writePassword() {
